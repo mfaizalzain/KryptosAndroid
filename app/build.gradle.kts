7e11a16ca4988bae0c2a59fa86e02f4ff3d96cfd
Faizal Zain <faizalmzain@gmail.com>
2026-05-16 03:19:10 +0800

    Bump version to 1.0.7 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
             "706867595241-e3ck7u69mnp2dtgf38vpu22k4ic5pcv9.apps.googleusercontent.com")
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
     }
 
     signingConfigs {
